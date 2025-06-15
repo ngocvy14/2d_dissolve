0d71bc0dce286d3f160251931ab23defaaef145f
ngocvy14 <nvy1404@gmail.com>
2025-06-15 15:28:22 +0700

    BodyPhysics & DemoForceOnObject & GUI

--- Assets/Script/BodyPhysics.cs
+++ Assets/Script/BodyPhysics.cs
@@ -6,9 +6,11 @@ public class BodyPhysics : MonoBehaviour
     [SerializeField] private float mass = 1f;
     [SerializeField] private bool useGravity = true;
     [SerializeField] private Vector3 gravity = new Vector3(0, -9.81f, 0);
-
+    
     private Vector3 velocity = Vector3.zero;
     private Vector3 force = Vector3.zero;
+
+
     public void AddForce(Vector3 inForce)
     {
         force += inForce;
@@ -20,19 +22,22 @@ public class BodyPhysics : MonoBehaviour
         {
             ApplyGravity();
         }
+        
+        // Apply forces to the body
         ApplyForces();
-
+        
+        // Update position based on velocity
         transform.position += velocity * Time.deltaTime;
     }
 
     private void ApplyForces()
     {
         velocity += force * (Time.deltaTime / mass);
+        force = Vector3.zero;
     }
 
     private void ApplyGravity()
     {
         velocity += gravity * (Time.deltaTime / mass);
     }
-
 }
--- Assets/Script/GUI.cs
+++ Assets/Script/GUI.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class GUInterface : MonoBehaviour
 {
-    [SerializeField] private Rigidbody targetRigitbody;
     [SerializeField] private BodyPhysics bodyPhysics;
+    [SerializeField] private Rigidbody targetRigidbody;
+
     private void OnGUI()
     {
-        if (GUILayout.Button("Apply Force") && targetRigitbody != null)
+        if (GUILayout.Button("Apply Unity Force") && targetRigidbody != null)
         {
-            targetRigitbody.AddForce(transform.right * 10, ForceMode.Force);
+            targetRigidbody.AddForce(transform.right * 10, ForceMode.Force);
         }
+
         if (GUILayout.Button("Apply Custom Force") && bodyPhysics != null)
         {
             bodyPhysics.AddForce(transform.right * 10);
         }
     }
-
 }
